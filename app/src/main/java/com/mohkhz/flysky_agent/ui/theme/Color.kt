34520ba9f946f2cf046abce335100e8c81c4aef1
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2022-05-18 00:09:53 +0430

    the primary and error color changed

---
+++ app/src/main/java/com/mohkhz/flysky_agent/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.mohkhz.flysky_agent.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val primary = Color(0xff6B1FDC)
+val secondColor = Color(0xffDF1356)
+val type = Color(0xff313037)
+val default = Color(0xff707070)
---
+++ app/src/main/java/com/mohkhz/flysky_agent/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.mohkhz.flysky_agent.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.mohkhz.flysky_agent.R
+
+val kalameh_bold = FontFamily(
+    Font(R.font.kalameh_bold),
+)
+val kalameh_regular = FontFamily(
+    Font(R.font.kalameh_regular),
+)
+val kalameh_semi_bold = FontFamily(
+    Font(R.font.kalameh_semi_bold),
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
