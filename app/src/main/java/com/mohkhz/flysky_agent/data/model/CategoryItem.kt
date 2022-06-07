8897bbaed69aa7bb50e68bb633e5576a8a8c3005
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2022-06-07 21:42:53 +0430

    name  var removed chnaged

--- app/src/main/java/com/mohkhz/flysky_agent/data/model/CategoryItem.kt
+++ app/src/main/java/com/mohkhz/flysky_agent/data/model/CategoryItem.kt
@@ -2,26 +2,21 @@ package com.mohkhz.flysky_agent_support.data.model
 
 sealed class CategoryItem(
     var id: String?,
-    var name: String?,
     var perName: String?,
 ) {
-
     class PriorityCat(
         id: String?,
-        name: String?,
         perName: String?,
-    ) : CategoryItem(id, name, perName)
+    ) : CategoryItem(id, perName)
 
     class TicketStatusCat(
         id: String?,
-        name: String?,
         perName: String?,
-    ) : CategoryItem(id, name, perName)
+    ) : CategoryItem(id, perName)
 
     class TicketCat(
         id: String?,
-        name: String?,
         perName: String?,
-    ) : CategoryItem(id, name, perName)
+    ) : CategoryItem(id, perName)
 
 }
