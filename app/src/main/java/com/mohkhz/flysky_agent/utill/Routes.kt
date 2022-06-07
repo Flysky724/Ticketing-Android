2b0ff39113658bbaa2568ede48b5588e1d2f466a
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2022-06-07 21:40:55 +0430

    new routh added

--- app/src/main/java/com/mohkhz/flysky_agent/utill/Routes.kt
+++ app/src/main/java/com/mohkhz/flysky_agent/utill/Routes.kt
@@ -5,7 +5,8 @@ object Routes {
     const val LOGIN_SCREEN = "login_Screen"
     const val HOME_SCREEN = "home_Screen"
     const val CHOOSE_SCREEN = "choose_Screen"
-    const val NEW_TICKET_SCREEN = "newTicket_Screen"
+    const val NEW_AGENT_SCREEN = "newAgent_Screen"
     const val TICKET_SCREEN = "ticket_Screen"
+    const val PROFILE_SCREEN = "profile_Screen"
 
 }
